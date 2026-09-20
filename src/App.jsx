3debb589990f9f90385ed999cec96a3fe81b4dc9
ashraf-0012 <ashrafyahaya830@gmail.com>
2026-09-20 14:59:18 -0800

    Remove topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
-import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Topbar />
+    
       <Navbar />
       <Outlet />
       <Footer />
